9a6fcfca1e155e765ba141f20adf666069a3dd72
Ivan Petrushev <ivanatora@gmail.com>
2019-10-16 21:07:34 +0300

    generating random point in a polygon

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ OSRM_TOKEN = '5b3ce3597851110001cf62481a688d900d66418ba38fc6db24843fd5';
 MAPBOX_TOKEN = 'pk.eyJ1IjoiaXZhbmF0b3JhIiwiYSI6ImNrMXMxNWd2ejA4ZXQzbWthMnQ3cWsxdmoifQ.BiIeOPWFWTTRZvi_7RPGEA';
 
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 15);
+var mymap = L.map('mapid').setView([42.1510598276, 24.7412538528], 15);
 var begin = {lat: 0, lng: 0};
 var end = {lat: 0, lng: 0};
 var selectMode = null;
@@ -17,6 +17,7 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='
 
 var markerBegin = new L.marker({lat: 0, lng: 0}, {draggable: true}).addTo(mymap);
 var markerEnd = new L.marker({lat: 0, lng: 0}, {draggable: true}).addTo(mymap);
+var markerMiddle = null;
 var route = null;
 
 mymap.on('click', function (e) {
@@ -40,7 +41,19 @@ selectEnd = function () {
     selectMode = 'end';
 };
 
+makeMarkerMiddle = function () {
+    if (markerMiddle) {
+        mymap.removeLayer(markerMiddle);
+    }
+    var b = mymap.getBounds();
+    var polygon = L.polygon([b.getNorthEast(), b.getSouthEast(), b.getSouthWest(), b.getNorthWest()]);
+    var latLng = randomPointInPoly(polygon);
+    markerMiddle = new L.marker(latLng, {draggable: true}).addTo(mymap);
+};
+
 navigate = async function () {
+    makeMarkerMiddle();
+
     var url = 'https://api.openrouteservice.org/v2/directions/foot-walking/geojson';
 
     var response = await fetch(url, {
@@ -54,13 +67,35 @@ navigate = async function () {
             // profile: 'foot-walking',
             coordinates: [
                 [markerBegin.getLatLng().lng, markerBegin.getLatLng().lat],
+                [markerMiddle.getLatLng().lng, markerMiddle.getLatLng().lat],
                 [markerEnd.getLatLng().lng, markerEnd.getLatLng().lat]
             ]
         })
-    })
+    });
     var json = await response.json();
     if (route) {
         mymap.removeLayer(route);
     }
     route = L.geoJSON(json.features).addTo(mymap);
+};
+
+randomPointInPoly = function(polygon) {
+    var bounds = polygon.getBounds();
+    var x_min  = bounds.getEast();
+    var x_max  = bounds.getWest();
+    var y_min  = bounds.getSouth();
+    var y_max  = bounds.getNorth();
+
+    var lat = y_min + (Math.random() * (y_max - y_min));
+    var lng = x_min + (Math.random() * (x_max - x_min));
+
+    var point  = turf.point([lng, lat]);
+    var poly   = polygon.toGeoJSON();
+    var inside = turf.inside(point, poly);
+
+    if (inside) {
+        return L.latLng(lat, lng);
+    } else {
+        return randomPointInPoly(polygon)
+    }
 };
