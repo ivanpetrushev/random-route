afb95e246fc54c78b8d807aa83df8faceaa1b0db
Ivan Petrushev <ivanatora@gmail.com>
2019-10-16 20:38:04 +0300

    simple navigation and showing result on map

--- scripts.js
+++ scripts.js
@@ -17,6 +17,7 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='
 
 var markerBegin = new L.marker({lat: 0, lng: 0}, {draggable: true}).addTo(mymap);
 var markerEnd = new L.marker({lat: 0, lng: 0}, {draggable: true}).addTo(mymap);
+var route = null;
 
 mymap.on('click', function (e) {
     if (selectMode === 'begin') {
@@ -28,7 +29,6 @@ mymap.on('click', function (e) {
 });
 
 navigator.geolocation.getCurrentPosition(function(position) {
-    console.log('getCurrentPosition', position);
     mymap.setView({lat: position.coords.latitude, lng: position.coords.longitude});
 });
 
@@ -38,4 +38,29 @@ selectBegin = function () {
 
 selectEnd = function () {
     selectMode = 'end';
+};
+
+navigate = async function () {
+    var url = 'https://api.openrouteservice.org/v2/directions/foot-walking/geojson';
+
+    var response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'Accept': 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
+            'Authorization': OSRM_TOKEN
+        },
+        body: JSON.stringify({
+            // profile: 'foot-walking',
+            coordinates: [
+                [markerBegin.getLatLng().lng, markerBegin.getLatLng().lat],
+                [markerEnd.getLatLng().lng, markerEnd.getLatLng().lat]
+            ]
+        })
+    })
+    var json = await response.json();
+    if (route) {
+        mymap.removeLayer(route);
+    }
+    route = L.geoJSON(json.features).addTo(mymap);
 };
